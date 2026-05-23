bd05c70f2ba14faab2d62bbd0b7902f39fb24529
Jonas Jönsson <jonas.jonsson@live.com>
2026-05-23 15:39:21 +0200

    Fixed an issue where create board caused the existing boards to expand.

--- src/components/boards/BoardElement.tsx
+++ src/components/boards/BoardElement.tsx
@@ -70,12 +70,12 @@ export const BoardElement = ({ name, description, id }: Board) => {
   }
 
   return (
-    <Card className="relative flex flex-1 items-center bg-(--color-surface)">
+    <Card className="relative flex bg-(--color-surface)">
       <div
         onClick={() => navigate(`/boards/${id}`)}
-        className="flex flex-1 cursor-pointer p-3 px-3 hover:bg-(--color-hover)"
+        className="flex flex-1 cursor-pointer items-start p-3 px-3 hover:bg-(--color-hover)"
       >
-        <div className="flex flex-1 flex-col gap-0.5">
+        <div className="flex flex-1 flex-col gap-2">
           <h2 className="max-w-9/10 text-lg wrap-anywhere">{name}</h2>
           {description && (
             <p className="text-sm wrap-anywhere text-(--color-text-muted)">
--- src/routes/BoardListView.tsx
+++ src/routes/BoardListView.tsx
@@ -5,7 +5,7 @@ export const BoardListView = () => {
   const { boards } = useBoardStore();
 
   return (
-    <div className="grid grid-cols-5 gap-3">
+    <div className="grid grid-cols-5 items-start gap-3">
       {boards.map((board) => (
         <BoardElement key={board.id} {...board} />
       ))}
